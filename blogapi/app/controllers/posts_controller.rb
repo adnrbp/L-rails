46350211ce583f1b245841619075ac568214180c
adnrbp <adnrbp@users.noreply.github.com>
2019-09-30 21:27:40 -0500

    [Upd] Post Controller: Create and update posts with auth user

--- blogapi/app/controllers/posts_controller.rb
+++ blogapi/app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
 		render json: {error: e.message}, status: :internal_error
 	end
 
+	rescue_from ActiveRecord::RecordNotFound do |e|
+		render json: {error: e.message}, status: :not_found
+	end
+
 	rescue_from ActiveRecord::RecordInvalid do |e|
 		render json: {error: e.message}, status: :unprocessable_entity
 	end
@@ -34,29 +38,30 @@ class PostsController < ApplicationController
 
 	# POST /posts
 	def create
-		@post = Post.create!(create_params)
+		@post = Current.user.posts.create!(create_params)
 		render json: @post, status: :created
 	end
 
 	# PUT /posts{id}
 	def update
-		@post = Post.find(params[:id])
+		@post = Current.user.posts.find(params[:id])
 		@post.update!(update_params)
 		render json: @post, status: :ok
+
 	end
 
 
 	private
 
 	def create_params
-		params.require(:post).permit(:title, :content, :published, :user_id)
+		params.require(:post).permit(:title, :content, :published)
 	end
 	
 	def update_params
 		params.require(:post).permit(:title, :content, :published)
 	end
 
-	def authenticate_user
+	def authenticate_user!
 		token_regex = /Bearer (\w+)/
 		# Read auth Header
 		headers = request.headers
--- blogapi/spec/requests/private_posts_spec.rb
+++ blogapi/spec/requests/private_posts_spec.rb
@@ -8,10 +8,14 @@ RSpec.describe "Posts with authentication", type: :request do
 	# Factories for Posts
 	let!(:user_post) {create(:post, user_id: user.id)}
 	let!(:other_user_post) {create(:post, user_id: other_user.id, published: true)}
+	let!(:user_post_draft) {create(:post, user_id: user.id, published: false)}
 	let!(:other_user_post_draft) {create(:post, user_id: other_user.id, published: false)}
 	# Fixtures for Auth Headers with tokens
 	let!(:auth_headers) { { 'Authorization' => "Bearer #{user.auth_token}" } }
   	let!(:other_auth_headers) { { 'Authorization' => "Bearer #{other_user.auth_token}" } }
+  	# Fixtures for request body
+  	let!(:create_post_params) { { "post" => {"title" => "title", "content" => "content", "published" => true}} }
+  	let!(:update_post_params) { { "post" => {"title" => "title", "content" => "content", "published" => true}} }
 
 
 	describe "Detail: GET /posts/{id}" do
@@ -46,17 +50,74 @@ RSpec.describe "Posts with authentication", type: :request do
 
 
 			end
-			context "when requesting user's post" do
-
-			end
 		end
 	end
 
 	describe "Create: GET /posts" do
+		# with auth: Create
+		context "with valid auth" do
+			before { post "/posts", params: create_post_params, headers: auth_headers}
+			
+			context "payload" do
+				subject { payload }
+				it { is_expected.to include(:id, :title, :content, :published, :author) }
+			end
+			context "response" do
+				subject { response }
+				it { is_expected.to have_http_status(:created) }
+			end
+		end
+		# without auth: !Create -> 401
+		context "without auth" do
+			before { post "/posts", params: create_post_params}
+			
+			context "payload" do
+				subject { payload }
+				it { is_expected.to include(:error) }
+			end
+			context "response" do
+				subject { response }
+				it { is_expected.to have_http_status(:unauthorized) }
+			end
+		end
 
 	end
 
 	describe "Update: PUT /posts" do
+		# with auth:
+			# update own post
+			# !update others post -> 401
+		context "with valid auth" do
+			context "when updating users's post" do
+				before { put "/posts/#{user_post.id}", params: update_post_params, headers: auth_headers}
+
+				context "payload" do
+					subject { payload }
+					it { is_expected.to include(:id, :title, :content, :published, :author) }
+					it { expect(payload[:id]).to eq(user_post.id) }
+				end
+				context "response" do
+					subject { response }
+					it { is_expected.to have_http_status(:ok) }
+				end
+			end
+
+			context "when updating other users's post" do
+				before { put "/posts/#{other_user_post.id}", params: update_post_params, headers: auth_headers}
+
+				context "payload" do
+					subject { payload }
+					it { is_expected.to include(:error) }
+				end
+				context "response" do
+					subject { response }
+					it { is_expected.to have_http_status(:not_found) }
+				end
+			end
+
+
+		end
+
 
 	end
 
