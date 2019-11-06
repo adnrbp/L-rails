47d1bc7f7d5bb770cbbb5411f0244add935214af
adnrbp <adnrbp@users.noreply.github.com>
2019-11-05 21:13:28 -0500

    [config] Doc Model Validations

---
+++ ourdocs/spec/models/doc_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Doc, type: :model do
+    describe 'columns' do
+        it { is_expected.to have_db_column(:title)}
+        it { is_expected.to have_db_column(:content)}
+        #it { should have_db_column(:support)}
+    end
+
+    describe "validations" do 
+        it "validate presence of required fields" do
+            should validate_presence_of(:title)
+            should validate_presence_of(:content)
+        end
+    end
+end
