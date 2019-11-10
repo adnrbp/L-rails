4397e23ad4fa9411b7b21bff94727d1f9892ed5b
adnrbp <adnrbp@users.noreply.github.com>
2019-11-09 21:40:29 -0500

    [config] Devise Basic

--- ourdocs/spec/models/doc_spec.rb
+++ ourdocs/spec/models/doc_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Doc, type: :model do
             should validate_presence_of(:content)
         end
     end
+
+    # it "considers a doc with no tasks to be done" do
+    #     # Arrange
+    #     doc = Doc.new
+    #     # Assert
+    #     expect(doc.done?).to be_truthy
+    # end
+    # it "knows that a doc with an incomplete task is not done" do
+    #     # Arrange
+    #     doc = Doc.new
+    #     task = Task.new
+    #     # Act
+    #     doc.tasks << task
+    #     # Assert
+    #     expect(doc.done?).to be_falsy
+    # end
+
+
 end
